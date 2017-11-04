89794e7a348e1d1aebca4cf0cffd0fefa7a826ee
Stephen Dolan <stephen.dolan@cl.cam.ac.uk>
2017-11-04 16:01:30 +0000

    some lifting functions

--- runtime/ppx_stage.ml
+++ runtime/ppx_stage.ml
@@ -5,3 +5,31 @@ module IdentMap = IdentMap
 
 let print = Internal.print
 let run = Internal.run
+
+let source x = Internal.source x Internal.Renaming.empty
+
+
+module Lift = struct
+  open Parsetree
+  open Ast_helper
+
+  let lift c p = Internal.Ppx_stage_internal ((fun env -> c), (fun ren -> p))
+
+  let int x : int code =
+    lift x (Exp.constant (Pconst_integer (string_of_int x, None)))
+  let int32 x : Int32.t code =
+    lift x (Exp.constant (Pconst_integer (Int32.to_string x, Some 'l')))
+  let int64 x : Int64.t code =
+    lift x (Exp.constant (Pconst_integer (Int64.to_string x, Some 'L')))
+  let bool x : bool code =
+    lift x (Exp.construct (Location.mknoloc (Longident.Lident (string_of_bool x))) None)
+  let float x : float code =
+    (* OCaml's string_of_float is a bit broken *)
+    let s = string_of_float x in
+    if float_of_string s = x then
+      lift x (Exp.constant (Pconst_float (s, None)))
+    else
+      lift x (Exp.constant (Pconst_float (Printf.sprintf "%h" x, None)))
+  let string x : string code =
+    lift x (Exp.constant (Pconst_string (x, None)))
+end
